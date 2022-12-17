8cea9ab48ecc2c1e4adb7c0620a35f7b6bf7580f
Chris Mayes <cmayes@cmay.es>
2022-12-16 17:42:24 -0700

    3: Fill in test coverage.

--- tests/test_playlists.py
+++ tests/test_playlists.py
@@ -3,7 +3,7 @@ import os
 import unittest
 from unittest.mock import MagicMock
 
-from spotcrates.playlists import Playlists
+from spotcrates.playlists import Playlists, PlaylistException
 
 
 def file_json(file_loc):
@@ -13,9 +13,25 @@ def file_json(file_loc):
 
 DATA_DIR = os.path.join(os.path.dirname(__file__), 'data')
 PLAYLIST_LIST = file_json(os.path.join(DATA_DIR, 'playlists.json'))
+PLAYLIST_NO_DAILY_LIST = file_json(os.path.join(DATA_DIR, 'playlists_no_daily.json'))
 TRACKS_DAILY1 = file_json(os.path.join(DATA_DIR, 'tracks_daily1.json'))
 TRACKS_OVERPLAYED = file_json(os.path.join(DATA_DIR, 'tracks_overplayed.json'))
 TRACKS_TARGET = file_json(os.path.join(DATA_DIR, 'tracks_target.json'))
+
+
+def get_canned_tracks(*args, **kwargs):
+    playlist_id = args[0]
+    if playlist_id == '37i9dQZF1E37hnawmowyJn':
+        return TRACKS_DAILY1
+    elif playlist_id == '0y8aCYE2OsnLzzxtqcDGf8':
+        return TRACKS_OVERPLAYED
+    elif playlist_id == '1JJB9ICuIoE6aD4jg9vgmV':
+        return TRACKS_TARGET
+    else:
+        raise Exception(f"Unhandled tracks ID {playlist_id}")
+
+
+
 
 
 class PlaylistTestCase(unittest.TestCase):
@@ -32,16 +48,6 @@ class PlaylistTestCase(unittest.TestCase):
     def test_append_daily_mix(self):
         self.spotify.current_user_playlists.return_value = {'items': PLAYLIST_LIST}
 
-        def get_canned_tracks(*args, **kwargs):
-            playlist_id = args[0]
-            if playlist_id == '37i9dQZF1E37hnawmowyJn':
-                return TRACKS_DAILY1
-            elif playlist_id == '0y8aCYE2OsnLzzxtqcDGf8':
-                return TRACKS_OVERPLAYED
-            elif playlist_id == '1JJB9ICuIoE6aD4jg9vgmV':
-                return TRACKS_TARGET
-            else:
-                raise Exception(f"Unhandled tracks ID {playlist_id}")
         self.spotify.playlist_items.side_effect = get_canned_tracks
 
         self.spotify.next.return_value = None
@@ -50,4 +56,58 @@ class PlaylistTestCase(unittest.TestCase):
 
         self.spotify.playlist_add_items.assert_called_with('1JJB9ICuIoE6aD4jg9vgmV', ['3DrlHWCoFqHQYGwE8MWsuv'])
 
+    def test_append_daily_mix_no_target(self):
+        self.spotify.current_user_playlists.return_value = {'items': PLAYLIST_LIST}
+
+        with self.assertRaises(PlaylistException):
+            self.playlists.append_daily_mix(target_list_name="missing_playlist")
+
+    # No dailies
+    def test_append_daily_mix_no_dailies(self):
+        self.spotify.current_user_playlists.return_value = {'items': PLAYLIST_NO_DAILY_LIST}
+
+        self.spotify.playlist_items.side_effect = get_canned_tracks
+
+        self.spotify.next.return_value = None
+
+        self.playlists.append_daily_mix()
+
+        self.spotify.playlist_add_items.assert_not_called()
+
     # Paged tracks
+
+    def test_append_daily_mix_paged(self):
+        self.spotify.current_user_playlists.return_value = {'items': PLAYLIST_LIST}
+
+        self.spotify.playlist_items.side_effect = get_canned_tracks
+
+        def get_next_page(*args, **kwargs):
+            page = args[0]
+            if page == TRACKS_TARGET:
+                return TRACKS_DAILY1
+            else:
+                return None
+
+        self.spotify.next.side_effect = get_next_page
+
+        self.playlists.append_daily_mix()
+
+        self.spotify.playlist_add_items.assert_not_called()
+
+    def test_append_daily_mix_paged_tracks(self):
+        self.spotify.current_user_playlists.return_value = {'items': PLAYLIST_LIST}
+
+        self.spotify.playlist_items.side_effect = get_canned_tracks
+
+        def get_next_page(*args, **kwargs):
+            page = args[0]
+            if page == TRACKS_DAILY1:
+                return TRACKS_TARGET
+            else:
+                return None
+
+        self.spotify.next.side_effect = get_next_page
+
+        self.playlists.append_daily_mix()
+
+        self.spotify.playlist_add_items.assert_called_with('1JJB9ICuIoE6aD4jg9vgmV', ['3DrlHWCoFqHQYGwE8MWsuv'])
